71f95c5194b5816d3e1ceb38f6c052ef0fa809c9
ikyya <jrd061901@gmail.com>
2025-09-08 21:25:50 +0700

    Update token list

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8340781099:AAFbc7aC-TLyBPKLaHizNKrN9ac3v6xOBqE",
-    "8410095485:AAGm_fnZUyg6g7oJiaMqE6F8pdWIK_R6qv4"
+    "8410095485:AAGm_fnZUyg6g7oJiaMqE6F8pdWIK_R6qv4",
+    "8288855606:AAEdMUjhKx8OHwATVVFk-3TJis0Rp5h4qw0"
   ]
 }
